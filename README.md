62612ea2df2ebd6745e1ced574beb063831ae674
Dave Winer <dave.winer@gmail.com>
2022-09-04 09:45:56 -0400

    v0.4.7

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ Read and write files to a GitHub repository.
 
 ### Updates
 
+5/4/21; 11:20:17 AM by DW
+
+Added getDirectory function.
+
 12/10/20 by DW
 
 This is when GH's <a href="https://developer.github.com/changes/2019-11-05-deprecated-passwords-and-authorizations-api/">deprecation warnings</a> caught uip with me. The script that uploads bits from scripting.com to the <a href="https://github.com/scripting/Scripting-News/tree/master/blog">Scripting-News repo</a> stopped working 27 days ago. Now I want to figure out what to do to get it working again. 
--- davegithub.js
+++ davegithub.js
@@ -1,10 +1,11 @@
-var myVersion = "0.4.5", myProductName = "davegithub";  
+var myVersion = "0.4.7", myProductName = "davegithub";  
 
 const utils = require ("daveutils");
 const request = require ("request");
 
 exports.getFile = getFile; 
 exports.uploadFile = uploadFile; 
+exports.getDirectory = getDirectory; 
 
 function getFile (options, callback) {
 	var url = "https://api.github.com/repos/" + options.username + "/" + options.repo + "/contents/" + options.repoPath;
@@ -75,5 +76,67 @@ function uploadFile (options, callback) {
 				callback (err, response, body);
 				}
 			});
+		});
+	}
+function getDirectory (options, path, callback) {
+	function loadDirectory (theArray, parentpath, callback) {
+		function nextFile (ix) {
+			if (ix < theArray.length) {
+				var item = theArray [ix];
+				if (item.type == "dir") {
+					getDirectory (options, item.path, function (err, jstruct) {
+						if (jstruct !== undefined) { //no error
+							item.subs = jstruct;
+							}
+						nextFile (ix + 1);
+						});
+					}
+				else {
+					nextFile (ix + 1);
+					}
+				}
+			else {
+				callback ();
+				}
+			}
+		nextFile (0);
+		}
+	if (utils.beginsWith (path, "/")) {
+		path = utils.stringDelete (path, 1, 1);
+		}
+	var theRequest = {
+		method: "GET",
+		url: "https://api.github.com/repos/" + options.username + "/" + options.repo + "/contents/" + path,
+		headers: {
+			"User-Agent": options.userAgent
+			}
+		};
+	console.log ("getDirectory: path == " + path);
+	request (theRequest, function (err, response, body) { 
+		if (err) {
+			if (callback !== undefined) {
+				callback (err);
+				}
+			}
+		else {
+			try {
+				var jstruct = JSON.parse (body);
+				if (Array.isArray (jstruct)) { //it's a directory
+					loadDirectory (jstruct, path, function () {
+						callback (undefined, jstruct);
+						});
+					}
+				else {
+					if (callback !== undefined) {
+						callback (jstruct);
+						}
+					}
+				}
+			catch (err) {
+				if (callback !== undefined) {
+					callback (err);
+					}
+				}
+			}
 		});
 	}

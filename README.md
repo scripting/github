ca064696d7baead1b8cea1aacdf8e4f313158c78
Dave Winer <dave.winer@gmail.com>
2022-10-02 11:33:05 -0400

    v0.5.3

--- README.md
+++ README.md
@@ -4,11 +4,31 @@ Read and write files to a GitHub repository.
 
 ### Updates
 
-5/4/21; 11:20:17 AM by DW
+#### 10/2/22 by DW
+
+When I integrated the new queued github writer with the big app I'm working on it failed to upload the files. After much debugging this is what the problem was:
+
+1. The files it was uploading already existed. 
+
+2. The GET call that the upload needs to do was getting rate-limited.
+
+3. But you can increase the rate-limit by authenticating.
+
+4. Since the upload functionality has to authenticate, I had prior art to copy, so if the request to get the file has a username and password in the options table, we do authenticate.
+
+5. It worked. ;-)
+
+#### 10/1/22 by DW
+
+New option when uploading a file -- flUseQueue, if true we make sure they run one after the other, because that's how the GH server likes it.
+
+Bumped the version to 0.5.0 because this is a fairly big feature and it's worth boasting about a bit. No breakage! :-)
+
+#### 5/4/21 by DW
 
 Added getDirectory function.
 
-12/10/20 by DW
+#### 12/10/20 by DW
 
 This is when GH's <a href="https://developer.github.com/changes/2019-11-05-deprecated-passwords-and-authorizations-api/">deprecation warnings</a> caught uip with me. The script that uploads bits from scripting.com to the <a href="https://github.com/scripting/Scripting-News/tree/master/blog">Scripting-News repo</a> stopped working 27 days ago. Now I want to figure out what to do to get it working again. 
 
--- davegithub.js
+++ davegithub.js
@@ -1,4 +1,4 @@
-var myVersion = "0.4.7", myProductName = "davegithub";  
+var myVersion = "0.5.3", myProductName = "davegithub";  
 
 const utils = require ("daveutils");
 const request = require ("request");
@@ -6,6 +6,10 @@ const request = require ("request");
 exports.getFile = getFile; 
 exports.uploadFile = uploadFile; 
 exports.getDirectory = getDirectory; 
+
+var uploadQueue = new Array (); //10/1/22 by DW
+var ctCurrentUploads = 0;
+var idQueueThread = undefined;
 
 function getFile (options, callback) {
 	var url = "https://api.github.com/repos/" + options.username + "/" + options.repo + "/contents/" + options.repoPath;
@@ -16,9 +20,19 @@ function getFile (options, callback) {
 			"User-Agent": options.userAgent
 			}
 		};
+	
+	if ((options.username !== undefined) && (options.password !== undefined)) { //10/2/22 by DW
+		theRequest.auth = {
+			user: options.username,
+			pass: options.password,
+			sendImmediately: true
+			};
+		}
+	
 	request (theRequest, function (err, response, body) { 
 		var jstruct = undefined;
 		if (err) {
+			console.log ("davegithub.getFile: err.message == " + err.message);
 			if (callback !== undefined) {
 				callback (err);
 				}
@@ -39,7 +53,27 @@ function getFile (options, callback) {
 			}
 		});
 	}
-function uploadFile (options, callback) {
+
+function checkQueryQueue () { //10/1/22 by DW
+	if (uploadQueue.length == 0) { //we can kill the thread
+		let id = idQueueThread;
+		idQueueThread = undefined;
+		clearInterval (id);
+		}
+	else {
+		if (ctCurrentUploads < 1) {
+			const jstruct = uploadQueue.shift ();
+			ctCurrentUploads++;
+			doUpload (jstruct.options, function (err, response, body) {
+				ctCurrentUploads--;
+				if (jstruct.callback !== undefined) {
+					jstruct.callback (err, response, body);
+					}
+				});
+			}
+		}
+	}
+function doUpload (options, callback) {
 	var bodyStruct = { 
 		message: options.message,
 		committer: {
@@ -49,11 +83,10 @@ function uploadFile (options, callback) {
 		content: Buffer.from (options.data).toString ("base64")
 		};
 	getFile (options, function (err, data, jstruct) {
+		var url = "https://api.github.com/repos/" + options.username + "/" + options.repo + "/contents/" + options.repoPath;
 		if (jstruct !== undefined) {
 			bodyStruct.sha = jstruct.sha;
 			}
-		var url = "https://api.github.com/repos/" + options.username + "/" + options.repo + "/contents/" + options.repoPath;
-		
 		var theRequest = {
 			method: "PUT",
 			url: url,
@@ -70,7 +103,7 @@ function uploadFile (options, callback) {
 			};
 		request (theRequest, function (err, response, body) { 
 			if (err) {
-				console.log ("uploadFile: err.message == " + err.message);
+				console.log ("doUpload: err.message == " + err.message);
 				}
 			if (callback !== undefined) {
 				callback (err, response, body);
@@ -78,6 +111,21 @@ function uploadFile (options, callback) {
 			});
 		});
 	}
+function startQueryQueue () {
+	idQueueThread = setInterval (checkQueryQueue, 100); //every tenth second
+	}
+function uploadFile (options, callback) { //10/1/22 by DW
+	if (options.flUseQueue) { //10/1/22 by DW
+		if (idQueueThread === undefined) {
+			startQueryQueue ();
+			}
+		uploadQueue.push ({options, callback});
+		}
+	else {
+		doUpload (options, callback); //do it immediately
+		}
+	}
+
 function getDirectory (options, path, callback) {
 	function loadDirectory (theArray, parentpath, callback) {
 		function nextFile (ix) {
